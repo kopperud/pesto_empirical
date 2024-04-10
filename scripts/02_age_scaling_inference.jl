3cfe11c85e4e375fa2448d1d6a582ed2c0abec92
Bjorn Tore Kopperud <kopperud@protonmail.com>
2024-04-10 10:58:08 +0200

    script for age scalinge ffect on hpc

--- scripts/02_age_scaling_inference.jl
+++ scripts/02_age_scaling_inference.jl
@@ -11,9 +11,7 @@ using ProgressMeter
 These are the outputs I want for the inference:
     * Newick string
     * N matrix (3-dim)
-    * Tip rates ??
     * Branch rates CSV file
-    * Constant-rate Birth-Death Process estimates
     * Estimate for η
 =#
 
@@ -22,65 +20,73 @@ n_iters = 500
 n_heights = length(tree_heights)
 
 prog = ProgressMeter.Progress(n_iters * n_heights; desc = "Inference: ");
-for i in 444:n_iters
-    for h in tree_heights[7:end]
-        #println("h = ", h, "\t -- \t i = ", i)
+for i in 1:n_iters
+    for h in tree_heights[1:end]
         phy = readtree(string("data/simulations/age_scaling_effect/h", Int64(h), "_", i ,".tre"))
         ρ = 1.0
         data = SSEdata(phy, ρ);
 
-        λml, μml = estimate_constant_bdp(data)
+        upper = [0.4, 2.0, 1.0]
 
-        H = 0.587
-        n = 5
+        try
+            optres, model, n_attempts = optimize_hyperparameters(data; upper = upper, n_attempts = 10)
 
-        dλ = LogNormal(log(λml), H)
-        dμ = LogNormal(log(µml), H)
+            g,h = logistic(upper, 0.5)
 
-        λquantiles = make_quantiles(dλ, n)
-        µquantiles = make_quantiles(dμ, n)
-        λ, μ = allpairwise(λquantiles, µquantiles)
+            x = g(optres.minimizer)
+            μml = sum(x[1:2])
+            λml = sum(x)
 
-        ηml = optimize_eta(λ, µ, data);
-        treelength = sum(data.branch_lengths);
-        model = SSEconstant(λ, μ, ηml);
+            ntip = length(data.tiplab)
 
-        Ds, Fs = backwards_forwards_pass(model, data);
-        Ss = ancestral_state_probabilities(data, Ds, Fs);
+            λ = model.λ
+            μ = model.μ
+            ηml = model.η
 
-        rates = tree_rates(data, model, Fs, Ss);
-        N = state_shifts(model, data, Ds, Fs);
-        #nshift = sum(N, dims = (2,3))[:,1,1];
-        #append!(nshift, 0.0)
-        #rates[!,"nshift"] = nshift
+            Ds, Fs = backwards_forwards_pass(model, data);
+            Ss = ancestral_state_probabilities(data, Ds, Fs);
 
-
-        #bf = posterior_prior_shift_odds(model,data)
-        #append!(bf, NaN)
-        #rates[!,"shift_bf"] = bf
-        #rates[!,"shift_bf_log"] = log10.(bf)
-
-        #tip_rates(model, data, Ds, Fs)
+            rates = tree_rates(data, model, Fs, Ss);
+            N = state_shifts(model, data, Ds, Ss);
+            nshift = sum(N, dims = (2,3))[:,1,1];
+            append!(nshift, 0.0)
+            rates[!,"nshift"] = nshift
 
 
-        ## save data
-        fpath = string("output/simulations/age_scaling_effect/newick/h", Int64(h), "_", i ,".tre")
-        writenewick(fpath, data, rates)
+            bf = posterior_prior_shift_odds(model,data)
+            append!(bf, NaN)
+            rates[!,"shift_bf"] = bf
+            rates[!,"shift_bf_log"] = log10.(bf)
 
-        fpath = string("output/simulations/age_scaling_effect/rates/h", Int64(h), "_", i ,".csv")
-        CSV.write(fpath, rates)
+            ## save data
+            fpath = string("output/simulations/age_scaling_effect/newick/h", Int64(h), "_", i ,".tre")
+            writenewick(fpath, data, rates)
 
-        fpath = string("output/simulations/age_scaling_effect/jld2/h", Int64(h), "_", i ,".jld2")
-        Nsum = sum(N, dims = 1)[1,:,:]
-        save(fpath, 
-            "N", N,
-            "Nsum", Nsum,
-            "lambda", λ,
-            "mu", μ,
-            "muml", μml,
-            "lambdaml", λml,
-            "etaml", ηml,
-            "treelength", treelength)
+            fpath = string("output/simulations/age_scaling_effect/rates/h", Int64(h), "_", i ,".csv")
+            CSV.write(fpath, rates)
+
+            fpath = string("output/simulations/age_scaling_effect/jld2/h", Int64(h), "_", i ,".jld2")
+            Nsum = sum(N, dims = 1)[1,:,:]
+            save(fpath, 
+                "N", N,
+                "Nsum", Nsum,
+                "lambda", λ,
+                "mu", μ,
+                "muml", μml,
+                "lambdaml", λml,
+                "n_attempts", n_attempts,
+                "ntip", ntip,
+                "etaml", ηml,
+                "treelength", treelength)
+          
+        catch e
+            if e isa Pesto.ConvergenceException
+                continue
+            else
+                rethrow(e)
+            end
+        end
+        
         next!(prog)
     end
 end
